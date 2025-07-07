79b274c0ea826cf0f8faa8a2cc33c6890df2f429
dmaberlin1 <dmaberlin77@gmail.com>
2025-07-07 07:56:33 +0300

    feat: View,helpers

--- core/View.php
+++ core/View.php
@@ -6,25 +6,37 @@ class View
 {
 
     public string $layout;
-    public string $content='';
+    public string $content = '';
 
     public function __construct($layout)
     {
-        $this->layout=$layout;
+        $this->layout = $layout;
     }
 
-    public function render($view,$data=[],$layout=''):string
+    public function render($view, $data = [], $layout = '')
     {
-        extract($data);
-        $view_file=VIEWS."/{$view}.php";
-        if (is_file($view_file)){
+        extract($data, EXTR_SKIP);
+        $view_file = VIEWS . "/{$view}.php";
+        if (is_file($view_file)) {
+            //            буферизация выводы
             ob_start();
             require $view_file;
-            return ob_get_clean();
-        }else{
-//            app()->response->setResponseCode(400);
+            $this->content = ob_get_clean();
+        } else {
+            //            app()->response->setResponseCode(400);
             response(500);
             return view('Errors/500');
         }
+//        if (!$layout)
+//            Сработает при любом "пустом" значении:
+//
+//false, '', null, 0, '0', []
+        if ($layout === false) {
+            return  $this->content;
+        }
+
+        $layout_file_name = $layout ?: $this->layout;
+        $layout_file = VIEWS . "/layouts/{$layout_file_name}.php";
+        dd($layout_file);
     }
 }
